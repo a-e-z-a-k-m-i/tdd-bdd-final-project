3b01d1360d06ee7a4fea9935408b5458d836dd0f
a-e-z-a-k-m-i <qwnrzxbstcpue@duck.com>
2023-10-09 06:02:48 -0400

    Improve code coverage

--- service/common/error_handlers.py
+++ service/common/error_handlers.py
@@ -56,8 +56,6 @@ def not_found(error):
 @app.errorhandler(status.HTTP_405_METHOD_NOT_ALLOWED)
 def method_not_supported(error):
     """Handles unsupported HTTP methods with 405_METHOD_NOT_SUPPORTED"""
-    message = str(error)
-    app.logger.warning(message)
     return (
         jsonify(
             status=status.HTTP_405_METHOD_NOT_ALLOWED,
@@ -86,8 +84,6 @@ def mediatype_not_supported(error):
 @app.errorhandler(status.HTTP_500_INTERNAL_SERVER_ERROR)
 def internal_server_error(error):
     """Handles unexpected server error with 500_SERVER_ERROR"""
-    message = str(error)
-    app.logger.error(message)
     return (
         jsonify(
             status=status.HTTP_500_INTERNAL_SERVER_ERROR,
